mguentner/rockbox

commit 6f6e7c4dfbd8e72b04ab2dffb60202e2934d4709

libwma: void pointer voodoo to get rid ov strict aliasing warnings
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@27463 a1c6a512-1295-4272-9138-f99709370657

apps/codecs/libwma/wmadeci.c
@@ -587,8 +587,8 @@ static inline fixed32 pow_m1_4(WMADecodeContext *s, fixed32 x)
     m = (u.v >> (23 - LSP_POW_BITS)) & ((1 << LSP_POW_BITS) - 1);
     /* build interpolation scale: 1 <= t < 2. */
     t.v = ((u.v << LSP_POW_BITS) & ((1 << 23) - 1)) | (127 << 23);
-    a = s->lsp_pow_m_table1[m];
-    b = s->lsp_pow_m_table2[m];
+    a = ((fixed32*)s->lsp_pow_m_table1)[m];
+    b = ((fixed32*)s->lsp_pow_m_table2)[m];
 
     /* lsp_pow_e_table contains 32.32 format */
     /* TODO:  Since we're unlikely have value that cover the whole
@@ -617,16 +617,16 @@ static void wma_lsp_to_curve_init(WMADecodeContext *s, int frame_len)
     b = itofix32(1);
     int ix = 0;
 
-    s->lsp_pow_m_table1 = (fixed32*)&vlcbuf3[0];
-    s->lsp_pow_m_table2 = (fixed32*)&vlcbuf3[VLCBUF3SIZE];
+    s->lsp_pow_m_table1 = &vlcbuf3[0];
+    s->lsp_pow_m_table2 = &vlcbuf3[VLCBUF3SIZE];
 
     /*double check this later*/
     for(i=(1 << LSP_POW_BITS) - 1;i>=0;i--)
     {
         m = (1 << LSP_POW_BITS) + i;
         a = pow_a_table[ix++]<<4;
-        s->lsp_pow_m_table1[i] = 2 * a - b;
-        s->lsp_pow_m_table2[i] = b - a;
+        ((fixed32*)s->lsp_pow_m_table1)[i] = 2 * a - b;
+        ((fixed32*)s->lsp_pow_m_table2)[i] = b - a;
         b = a;
     }
 

apps/codecs/libwma/wmadec.h
@@ -147,8 +147,8 @@ typedef struct WMADecodeContext
     fixed32 noise_mult; /* XXX: suppress that and integrate it in the noise array */
     /* lsp_to_curve tables */
     fixed32 lsp_cos_table[BLOCK_MAX_SIZE];
-    fixed32 *lsp_pow_m_table1;
-    fixed32 *lsp_pow_m_table2;
+    void *lsp_pow_m_table1;
+    void *lsp_pow_m_table2;
 
     /* State of current superframe decoding */
     int bit_offset;

commit 39a9c25485cb4cda9a4d4e0cb7945fb4f451da68

Fix a bug introduced in r27463 that caused the line spectral pair look up tables to overwrite the noise coding VLC tables.  Should fix decoding of files that have both noise coding and line spectral pairs, which are usually very low bitrate WMAs.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@30220 a1c6a512-1295-4272-9138-f99709370657

apps/codecs/libwma/wmadeci.c
@@ -621,7 +621,7 @@ static void wma_lsp_to_curve_init(WMADecodeContext *s, int frame_len)
     int ix = 0;
 
     s->lsp_pow_m_table1 = &vlcbuf3[0];
-    s->lsp_pow_m_table2 = &vlcbuf3[VLCBUF3SIZE];
+    s->lsp_pow_m_table2 = &vlcbuf3[1<<LSP_POW_BITS];
 
     /*double check this later*/
     for(i=(1 << LSP_POW_BITS) - 1;i>=0;i--)